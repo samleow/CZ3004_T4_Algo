61812e1901f25da873ca1256fd7e2f9e9aab2d3d
MacDboi <epiclol1998@gmail.com>
2021-08-31 22:10:19 +0800

    made a list of objects

--- src/readinput.java
+++ src/readinput.java
@@ -1,18 +1,43 @@
 package src;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import src.Position.Orientation;
 
 public class readinput{
     public readinput(){
         try{
             File input = new File("src/input.txt"); //specify relative to src folder
             Scanner inputreader = new Scanner(input);
+            List<Position> obstacles = new ArrayList<Position>(); 
             while (inputreader.hasNextLine())
             {
                 String data = inputreader.nextLine();
                 String[] obstacledata = data.split("\\s*,\\s*");
-                System.out.println(obstacledata[0]); //create obstacles here
+                if (obstacledata[2].equals("NORTH"))
+                {
+                    obstacles.add(new Position(Integer.parseInt(obstacledata[0]), Integer.parseInt(obstacledata[1]), Orientation.NORTH));
+                }
+                else if (obstacledata[2].equals("EAST"))
+                {
+                    obstacles.add(new Position(Integer.parseInt(obstacledata[0]), Integer.parseInt(obstacledata[1]), Orientation.EAST));
+                }
+                else if (obstacledata[2].equals("WEST"))
+                {
+                    obstacles.add(new Position(Integer.parseInt(obstacledata[0]), Integer.parseInt(obstacledata[1]), Orientation.WEST));
+                }
+                else if (obstacledata[2].equals("SOUTH"))
+                {
+                    obstacles.add(new Position(Integer.parseInt(obstacledata[0]), Integer.parseInt(obstacledata[1]), Orientation.SOUTH));
+                }
+                    
+            }
+            for (Position ac:obstacles)
+            {
+                System.out.println(ac.toString());
             }
         } catch (FileNotFoundException e) {
             System.out.println("An error has occurred.");
